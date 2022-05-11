039b37463014c5dc1c20449c34771b92714b77df
carlvskansas <48308402+carlgwsmith@users.noreply.github.com>
2022-05-10 20:07:13 -0500

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,16 @@ const connectDB = require('./config/dbConn');
 const PORT = process.env.PORT || 3500;
 
 // Cross Origin Resource Sharing
-app.use(cors());
+// app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 connectDB();
 
 // built-in middleware to handle urlencoded form data
